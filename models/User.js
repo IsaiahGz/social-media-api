4d3acb2d19be3c2300a9c65a989ae951ec298293
IsaiahGz <isaiahgala99@gmail.com>
2023-05-05 15:59:32 -0400

    Change thoughts and friends to arrays

--- models/User.js
+++ models/User.js
@@ -15,14 +15,18 @@ const userSchema = new Schema(
 			required: true,
 			match: [/.+@.+\..+/, 'Please enter a valid e-mail address'],
 		},
-		thoughts: {
-			type: Schema.Types.ObjectId,
-			ref: 'Thought',
-		},
-		friends: {
-			type: Schema.Types.ObjectId,
-			ref: 'User',
-		},
+		thoughts: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Thought',
+			},
+		],
+		friends: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'User',
+			},
+		],
 	},
 	{
 		toJSON: {
